54f2a9cac94d4ba7084ded821c4d965bc8ec983a
Lawrence Cushman <lawrencecushman@gmail.com>
2013-09-27 20:55:37 -0400

    First Commit!

---
+++ DiscountCalculator/src/lcushman/lab02/DiscountCalculator.java
@@ -0,0 +1,22 @@
+package lcushman.lab02;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DiscountCalculator extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_discount_calculator);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.discount_calculator, menu);
+		return true;
+	}
+
+}
