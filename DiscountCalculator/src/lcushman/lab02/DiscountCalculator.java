a5928b88cb811875f6a9da43176d735556e6121c
Lawrence Cushman <lawrencecushman@gmail.com>
2013-09-28 16:46:59 -0400

    Refactor java code. Simplify Action Listeners

--- DiscountCalculator/src/lcushman/lab02/DiscountCalculator.java
+++ DiscountCalculator/src/lcushman/lab02/DiscountCalculator.java
@@ -4,45 +4,37 @@ import java.text.DecimalFormat;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.graphics.Color;
+import android.content.Context;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
-public class DiscountCalculator extends Activity {
+/** The style of the ActionListener used to implement this version implements 
+the ActionListener in the activity. This avoids the extra class load and object 
+allocation.
+More Info: <a href="http://developer.android.com/guide/topics/ui/ui-events.html">
+Android Developers - UI Events
+</a>**/
+public class DiscountCalculator extends Activity implements OnEditorActionListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_discount_calculator);
 		
+		// Both edit fields utilize the same onEditorAction, which simply means, 
+		// if either field is edited, the result TextView will be updated.
+		final EditText price_View = (EditText) findViewById(R.id.price_decimalField);
+		price_View.setOnEditorActionListener(this);
+		
 		final EditText discount_View = (EditText) findViewById(R.id.discount_decimalField);
-		discount_View.setOnEditorActionListener(new OnEditorActionListener(){
-			
-			@Override
-			public boolean onEditorAction(TextView v, int actionId,	KeyEvent event) {
-				try {
-					//get the string from the text box
-					float discount = Float.parseFloat(v.getText().toString());
-					String price_String = ((EditText) findViewById(R.id.price_decimalField)).getText().toString();
-					float price = Float.parseFloat(price_String);
-					
-					Float result = (price - price*discount/100.0f); 
-					// format the float to two decimal places
-					String result_2dp = new DecimalFormat("##.00").format(result);
-					((TextView) findViewById(R.id.result_textView)).setText('$' + result_2dp);
-					//((EditText) findViewById(R.id.discount_decimalField)).requestFocus();
-					
-					
-				} catch (Exception e) {
-					((TextView) findViewById(R.id.result_textView)).setText("NOPE!");
-					
-				}
-				return false;
-			}
-		});
+		discount_View.setOnEditorActionListener(this);
+		
+		
+		
 	}
 
 	@Override
@@ -53,5 +45,28 @@ public class DiscountCalculator extends Activity {
 	}
 	
 	
-	
+	@Override
+	public boolean onEditorAction(TextView v, int actionId,	KeyEvent event) {
+		try {
+			// Get the string held in the price EditText View
+			String price_String = ((EditText) findViewById(R.id.price_decimalField)).getText().toString();
+			float price = Float.parseFloat(price_String);
+
+			// Get the string held in the discount EditText View
+			String discount_string = ((EditText) findViewById(R.id.discount_decimalField)).getText().toString();
+			float discount = Float.parseFloat(discount_string); // v is the view
+			
+			Float result = (price - price*discount/100.0f); // Calculate what you pay 
+			
+			// format the float to two decimal places
+			String result_2dp = new DecimalFormat("##.00").format(result);
+			((TextView) findViewById(R.id.result_textView)).setText('$' + result_2dp);
+		} catch (Exception e) {
+			((TextView) findViewById(R.id.result_textView)).setText("--");
+		}
+		InputMethodManager imm = (InputMethodManager)getSystemService(
+			      Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+		return false; // This will cause the keyboard to hide when 'Done' is clicked
+	}
 }
