fedd5ad307ab83db12f32b4fe8bea4e3029a2164
Lawrence Cushman <lawrencecushman@gmail.com>
2013-09-30 16:35:30 -0400

    - Refactor the code to allow multiple views with the same action

--- DiscountCalculator/src/lcushman/lab02/DiscountCalculator.java
+++ DiscountCalculator/src/lcushman/lab02/DiscountCalculator.java
@@ -12,9 +12,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
+
 /** The style of the ActionListener used to implement this version implements 
-the ActionListener in the activity. This avoids the extra class load and object 
-allocation.
+the Activity as the Action Listener. This avoids the extra class load and object 
+allocation, and allows the use of the same action for multiple Views.
 More Info: <a href="http://developer.android.com/guide/topics/ui/ui-events.html">
 Android Developers - UI Events
 </a>**/
@@ -25,16 +26,14 @@ public class DiscountCalculator extends Activity implements OnEditorActionListen
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_discount_calculator);
 		
-		// Both edit fields utilize the same onEditorAction, which simply means, 
-		// if either field is edited, the result TextView will be updated.
+		// Both edit fields below utilize the same onEditorAction, which simply means, 
+		// if either field is edited and submitted, via the 'Next' or 'Done' buttons,
+		// the result TextView will be updated.
 		final EditText price_View = (EditText) findViewById(R.id.price_decimalField);
 		price_View.setOnEditorActionListener(this);
 		
 		final EditText discount_View = (EditText) findViewById(R.id.discount_decimalField);
-		discount_View.setOnEditorActionListener(this);
-		
-		
-		
+		discount_View.setOnEditorActionListener(this);	
 	}
 
 	@Override
@@ -67,6 +66,6 @@ public class DiscountCalculator extends Activity implements OnEditorActionListen
 		InputMethodManager imm = (InputMethodManager)getSystemService(
 			      Context.INPUT_METHOD_SERVICE);
 		imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-		return false; // This will cause the keyboard to hide when 'Done' is clicked
+		return false; // This will cause the keyboard to hide when 'Done' or 'Next' is clicked
 	}
 }
